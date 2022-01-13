0782354f9c084ac12e136d787011bafc0e00fba3
Name-Artem <79202211833@yandex.ru>
2022-01-13 18:50:17 +0300

    fifth commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const first = document.querySelector('.first');
+const second = document.querySelector('.second');
+const third = document.querySelector('.third');
+
+first.onclick = function () {
+    console.log ('work');
+}
+
+second.onclick = function () {
+    console.log ('good');
+}
+
+third.onclick = function () {
+    console.log ('fine');
+}
